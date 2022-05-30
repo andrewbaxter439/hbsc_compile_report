2b2c7133211924c995e8e021511cf17f5bef3147
Andy Baxter <andrewbaxter439@gmail.com>
2022-05-30 20:04:18 +0100

    semi-working bar_by_cat function

--- R/functions.R
+++ R/functions.R
@@ -55,33 +55,69 @@ bar_by_cat <- function(var,
                        .data = school_dat) {
   var <- enquo(var)
   
-  p1 <- .data |>
+  df_sex <- .data |>
     group_by(sex) |>
     mutate(success = !!var %in% success) |>
-    summarise(prop = sum(success) / n()) |>
-    filter(!is.na(sex)) |> 
-    ggplot(aes(sex, prop, fill = sex)) +
-    geom_bar(stat = "identity") +
-    scale_fill_hbsc() +
-    scale_y_continuous("%", labels = percent, limits = c(0, 1))
+    summarise(numerator = sum(success),
+              denom = n()) |>
+    filter(!is.na(sex))
   
-  p2 <- .data |>
+  if (all(df_sex$numerator > 3) & all(df_sex$denom >= 7)) {
+    # * chart should not be created if there are ≤3 students in the numerator of
+    #   any variable.
+    # * only separate by sex if there are ≥7 girls AND ≥7 boys in the denominator
+    #   of any variable
+    
+    p1 <- df_sex |>
+      mutate(prop = numerator / denom) |>
+      ggplot(aes(sex, prop, fill = sex)) +
+      geom_bar(stat = "identity") +
+      scale_fill_hbsc() +
+      scale_y_continuous("%", labels = percent, limits = c(0, 1))
+    
+  } else if (all(df_sex$numerator > 3) & sum(df_sex$denom <= 14)) {
+    # * if there are ≤14 students, the chart should only present a single column
+    #   representing all students.
+    
+    p1 <- df_sex |>
+      summarise(prop = sum(numerator) / sum(denom)) |>
+      ggplot(aes("All pupils", prop)) +
+      geom_bar(stat = "identity") +
+      scale_fill_hbsc() +
+      scale_y_continuous("%", labels = percent, limits = c(0, 1))
+    
+  } else {
+    p1 <- ggplot() +
+      geom_text(aes(x = 1, y = 0.5, label = "Chart ommitted\ndue to low numbers"),
+                size = 12) +
+      scale_x_discrete(breaks = 1, labels = "") +
+      scale_y_continuous("%", labels = percent, limits = c(0, 1))
+    
+  }
+  
+  df_school <- .data |>
     group_by(grade) |>
     mutate(success = !!var %in% success) |>
-    summarise(prop = sum(success) / n()) |>
-    filter(!is.na(grade)) |> 
-    ggplot(aes(grade, prop, fill = grade)) +
+    summarise(numerator = sum(success),
+              denom = n()) |>
+    filter(!is.na(grade))
+  
+  if (all(df_school$numerator >= 7)) {
+    # * for secondary schools, only separate by year if there are ≥7 S2 AND ≥7 S4). 
+    
+    p2 <- df_school |> 
+      summarise(prop = sum(numerator) / sum(denom)) |>
+      ggplot(aes(grade, prop, fill = grade)) +
     geom_bar(stat = "identity") +
     scale_fill_hbsc() +
     scale_y_continuous("%", labels = percent, limits = c(0, 1), position = "right")
+  } else {
+    p2 <- NULL
+  }
   
   p1 + p2
 }
 
-# denoms - check all >= 7
-# school_dat |> 
-#   count()
+# test
+# bar_by_cat(health, c("Good", "Excellent"))
 
-#numerators - check all > 3
-# school_dat |> 
-  # summarise(sum = sum(health))
