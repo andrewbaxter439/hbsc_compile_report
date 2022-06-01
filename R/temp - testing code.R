27a63ac69a93b4ec7fb45c41c85ef2c58e21a8e5
Andy Baxter <andrewbaxter439@gmail.com>
2022-06-01 09:37:12 +0100

    added prompt to data input

---
+++ R/temp - testing code.R
@@ -0,0 +1,183 @@
+success  <- c("Good", "Excellent")
+.data <-  school_dat
+
+
+df_sex <- .data |>
+  group_by(sex) |>
+  mutate(success = health %in% success) |>
+  summarise(numerator = sum(success),
+            denom = n()) |>
+  filter(!is.na(sex))
+
+if (all(df_sex$numerator > 3) & all(df_sex$denom) >= 7) {
+  # * chart should not be created if there are ≤3 students in the numerator of
+  #   any variable.
+  # * only separate by sex if there are ≥7 girls AND ≥7 boys in the denominator
+  #   of any variable
+  
+  p1 <- df_sex |>
+    mutate(prop = numerator / denom) |>
+    ggplot(aes(sex, prop, fill = sex)) +
+    geom_bar(stat = "identity") +
+    scale_fill_hbsc() +
+    scale_y_continuous("%", labels = percent, limits = c(0, 1))
+  
+} else if (all(df_sex$numerator > 3) & sum(df_sex$denom <= 14)) {
+  # * if there are ≤14 students, the chart should only present a single column
+  #   representing all students.
+  
+  p1 <- df_sex |>
+    summarise(prop = sum(numerator) / sum(denom)) |>
+    ggplot(aes("All pupils", prop)) +
+    geom_bar(stat = "identity") +
+    scale_fill_hbsc() +
+    scale_y_continuous("%", labels = percent, limits = c(0, 1))
+  
+} else {
+  ggplot() +
+    geom_text(aes(x = 1, y = 0.5, label = "Chart ommitted\ndue to low numbers"),
+              size = 12) +
+    scale_x_discrete(breaks = 1, labels = "") +
+    scale_y_continuous("%", labels = percent, limits = c(0, 1))
+  
+}
+
+
+
+# test 2
+
+df_sex <- .data |>
+  group_by(sex) |>
+  mutate(success = health %in% success) |>
+  summarise(numerator = sum(success),
+            denom = n()) |>
+  filter(!is.na(sex))
+
+if (all(df_sex$numerator > 3) & all(df_sex$denom >= 7)) {
+  # * chart should not be created if there are ≤3 students in the numerator of
+  #   any variable.
+  # * only separate by sex if there are ≥7 girls AND ≥7 boys in the denominator
+  #   of any variable
+  
+  p1 <- df_sex |>
+    mutate(prop = numerator / denom) |>
+    ggplot(aes(sex, prop, fill = sex)) +
+    geom_bar(stat = "identity") +
+    scale_fill_hbsc() +
+    scale_y_continuous("%", labels = percent, limits = c(0, 1))
+  
+} else if (all(df_sex$numerator > 3) & sum(df_sex$denom <= 14)) {
+  # * if there are ≤14 students, the chart should only present a single column
+  #   representing all students.
+  
+  p1 <- df_sex |>
+    summarise(prop = sum(numerator) / sum(denom)) |>
+    ggplot(aes("All pupils", prop)) +
+    geom_bar(stat = "identity") +
+    scale_fill_hbsc() +
+    scale_y_continuous("%", labels = percent, limits = c(0, 1))
+  
+} else {
+  p1 <- ggplot() +
+    geom_text(aes(x = 1, y = 0.5, label = "Chart ommitted\ndue to low numbers"),
+              size = 12) +
+    scale_x_discrete(breaks = 1, labels = "") +
+    scale_y_continuous("%", labels = percent, limits = c(0, 1))
+  
+}
+
+df_school <- .data |>
+  group_by(grade) |>
+  mutate(success = health %in% success) |>
+  summarise(numerator = sum(success),
+            denom = n()) |>
+  filter(!is.na(grade))
+
+if (length(df_school$grade) == 2 & all(df_school$numerator >= 7)) {
+  # * for secondary schools, only separate by year if there are ≥7 S2 AND ≥7 S4). 
+  
+  p2 <- df_school |> 
+    summarise(prop = sum(numerator) / sum(denom)) |>
+    ggplot(aes(grade, prop, fill = grade)) +
+    geom_bar(stat = "identity") +
+    scale_fill_hbsc() +
+    scale_y_continuous("%", labels = percent, limits = c(0, 1), position = "right")
+  
+} else {
+  
+  p2 <- NULL
+  
+}
+
+p1 + p2
+
+
+
+# across cats graphs ------------------------------------------------------
+
+
+success <- c("More than once a week", "About every day")
+cat_labels <-
+  c(
+    "Headache",
+    "Stomach-ache",
+    "Backache",
+    "Dizziness",
+    "Feeling low",
+    "Feeling nervous",
+    "Feeling irritable",
+    "Sleep difficulties"
+  )
+
+
+bar_multiple_vars <- function(..., labels, success = c("More than once a week", "About every day"), .data = school_dat)
+  
+  vars <- enquos(...)
+
+school_dat |> 
+  select(headache, stomachache, backache, dizzy, feellow, nervous, irritable, sleepdificulty) |> 
+  summarise(across(everything(), ~ sum(.x %in% success)),
+            denom = n()) |> 
+  pivot_longer(-denom, names_to = "var", values_to = "n") |> 
+  mutate(censor = if_else(n < 3, 1, 0),
+         labels = cat_labels,
+         prop = n/denom) |> 
+  ggplot(aes(fct_inorder(labels), prop)) +
+  geom_bar_t(stat = "identity") +
+  scale_fill_hbsc() +
+  scale_x_discrete(guide = guide_axis(n.dodge = 2)) +
+  scale_y_continuous("%", labels = percent, limits = c(0, 1))
+
+
+# smoother as named list?
+varslist <- list(
+headache =   "Headache",
+stomachache =   "Stomach-ache",
+backache =   "Backache",
+dizzy =   "Dizziness",
+feellow =   "Feeling low",
+nervous =   "Feeling nervous",
+irritable =   "Feeling irritable",
+sleepdificulty =   "Sleep difficulties")
+
+bar_multiple_vars <- function(varslist = varslist, success = c("More than once a week", "About every day"), .data = school_dat) {
+  
+  .data |> 
+    select(!!!syms(names(varslist))) |> 
+    summarise(across(everything(), ~ sum(.x %in% success)),
+              denom = n()) |>
+    pivot_longer(-denom, names_to = "var", values_to = "n") |>
+    rowwise() |> 
+    mutate(censor = if_else(n < 3, 1, 0),
+           labels = varslist[[var]][1],
+           prop = n/denom) |>
+    ggplot(aes(fct_inorder(labels), prop)) +
+    geom_bar_t(stat = "identity") +
+    scale_fill_hbsc() +
+    scale_x_discrete(guide = guide_axis(n.dodge = 2)) +
+    scale_y_continuous("%", labels = percent, limits = c(0, 1))
+  
+}
+
+bar_multiple_vars(varslist = varslist)
+
