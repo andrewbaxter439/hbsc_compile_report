5cfc426eaf317e9166816772c2c9be093a98d085
Andy Baxter <andrewbaxter439@gmail.com>
2022-08-26 11:53:10 +0100

    Test lines for no-gender-split

--- R/August school reports to knit.R
+++ R/August school reports to knit.R
@@ -153,3 +153,36 @@ readxl::read_excel(
          .keep = "unused") |> 
   filter(SCHOOL_number != "066") |> 
   write_reports(template = "primary_report_template.Rmd", out_dir = file.path(out_dir, "Reports for checking August 2022"), folder = "Rerun reports with small numbers")
+
+
+# rerun for no gender split -----------------------------------------------
+
+readxl::read_excel(
+  file.path(out_dir, "Report overview JULY 2022.xlsx"),
+  sheet = "Primary school"
+) |>
+  select(school_name = Name, LA, id = `School iD`) |> 
+  mutate(SCHOOL_number = str_extract(id, "(?<=^P7)\\d{3}"),
+         .keep = "unused") |> 
+  filter(SCHOOL_number %in% c("020", "042", "037", "046")) |> 
+  write_reports(template = "primary_report_template.Rmd", out_dir = file.path(out_dir, "Reports for checking August 2022"), folder = "Test")
+
+readxl::read_excel(
+  file.path(out_dir, "Report overview 020822.xlsx"),
+  sheet = "Reports under consideration"
+) |>
+  select(school_name = Name, LA, id = `School iD`) |> 
+  mutate(SCHOOL_number = str_extract(id, "(?<=^P7)\\d{3}"),
+         .keep = "unused") |> 
+  filter(SCHOOL_number %in% c("006", "073", "013", "061", "048")) |> 
+  write_reports(template = "primary_report_template.Rmd", out_dir = file.path(out_dir, "Reports for checking August 2022"), folder = "Test")
+
+readxl::read_excel(
+  file.path(out_dir, "Report overview 020822.xlsx"),
+  sheet = "Reports under consideration"
+) |>
+  select(school_name = Name, LA, id = `School iD`) |> 
+  mutate(SCHOOL_number = str_extract(id, "(?<=^P7)\\d{3}"),
+         .keep = "unused") |> 
+  filter(SCHOOL_number != "066") |> 
+  write_reports(template = "primary_report_template.Rmd", out_dir = file.path(out_dir, "Reports for checking August 2022"), folder = "Test")
